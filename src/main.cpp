2d295fbd38b7639069d7181bb37dedb651c03c86
Samuel Fikadu <samuelfikadu94@gmail.com>
2025-10-11 15:25:26 -0400

    setup

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <renderer/core/CommandPool.h>
+#include <physics/DrawCommandGenerator.h>
+int main(){
+    std::cout << "hello, Vulkan!" << std::endl;
+    return 0;
+}
